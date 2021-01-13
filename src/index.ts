220441680c1e7dbd329d0350d1527d8361db45e3
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-13 23:06:53 +0000

    Added set method to User class with ability to only add optional props

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from "./models/User";
 
 const user = new User({name: 'Sunny', age: 33});
+user.set({name: 'Kim', age: 28});
 console.log(user.get('name'));
 console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -6,7 +6,12 @@ interface UserProps {
 export class User {
   constructor(private data: UserProps) {}
 
-  get = (propName: string): number | string => {
-    return this.data[propName];
+  get = (propName: string): string | number => {
+    return this.data[propName as keyof UserProps];
+  };
+
+  // Partial ensures that the props are optional here, but still ensures they are mandatory when creating new User
+  set = (update: Partial<UserProps>): void => {
+    Object.assign(this.data, update)
   };
 }
