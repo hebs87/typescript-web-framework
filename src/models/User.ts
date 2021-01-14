e74ea81bccea5a1f75be13f2600345c69735cf60
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-14 22:42:41 +0000

    Added events property to User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,6 +7,9 @@ interface UserProps {
 type Callback = () => {};
 
 export class User {
+  // Event object that holds keys that are strings with arrays of callback functions as their properties
+  events: {[key: string]: Callback[]} = {};
+
   constructor(private data: UserProps) {}
 
   get = (propName: string): string | number => {
@@ -16,5 +19,9 @@ export class User {
   // Partial ensures that the props are optional here, but still ensures they are mandatory when creating new User
   set = (update: Partial<UserProps>): void => {
     Object.assign(this.data, update)
+  };
+
+  on = (eventName: string, callback: Callback) => {
+
   };
 }
