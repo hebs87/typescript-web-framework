b1fbad8056c2f29213bca2301cd682f0e3b1be14
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-17 00:35:14 +0000

    Connected Attributes class in User class and initialised with attrs object

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import {Eventing} from "./Eventing";
 import {Sync} from "./Sync";
+import {Attributes} from "./Attributes";
 
 export interface UserProps {
   id?: number;
@@ -10,7 +11,15 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User {
+  // Initialise Eventing class to enable calling it from User
   public events: Eventing = new Eventing();
+  // Initialise Sync class to enable calling it from User
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  // Create attributes property
+  public attributes: Attributes<UserProps>;
 
+  constructor(private attrs: UserProps) {
+    // Initialise Attributes class to enable calling it from User, pass in attrs object
+    this.attributes = new Attributes<UserProps>(attrs);
+  };
 }
