be322b06aa166b462397d5393b641a85bc628c3a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-17 00:05:39 +0000

    Refactored Sync class methods functionality and hooked up to User class

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,27 +1,35 @@
-import axios, {AxiosResponse} from "axios";
+import axios, {AxiosResponse, AxiosPromise} from "axios";
 
+interface HasId {
+  id?: number;
+}
 
-export class Sync {
-  fetch = (): void => {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((res: AxiosResponse): void => {
-        console.log(res.data);
-        this.set(res.data);
-      })
-      .catch((error: AxiosResponse): void => {
-        console.log(error)
-      });
+// Generic class which accepts any argument type, but it extends HasId, which tells TypeScript that it will have an id
+export class Sync<T extends HasId> {
+  constructor(public rootUrl: string) {}
+
+  fetch = (id: number): AxiosPromise => {
+    // This method is only responsible for fetching the data. The User class will then be responsible for processing it
+    return axios.get(`${this.rootUrl}/${id}`);
   };
 
-  save = (): void => {
-    const id = this.get('id');
+  save = (data: T): AxiosPromise => {
+    const {id} = data;
 
     if (id) {
       // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
+      return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
       // post
-      axios.post('http://localhost:3000/users', this.data);
+      return axios.post(this.rootUrl, data);
     }
   };
 }
+
+// .then((res: AxiosResponse): void => {
+//   console.log(res.data);
+//   this.set(res.data);
+// })
+//   .catch((error: AxiosResponse): void => {
+//     console.log(error)
+//   });
