cc1caa7acabad04dc928fb229519cba9f5e1182e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-17 00:28:47 +0000

    Created Attributes class and moved and refactored User class get and set methods into it

---
+++ src/models/Attributes.ts
@@ -0,0 +1,15 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  // Function generics - K can only be one of the keys (key string value) of the relevant interface that is passed in
+  // The key will be the K string value
+  // The return type will be the return type of the matching key's type of the relevant interface that is passed in
+  get = <K extends keyof T>(key: K): T[K] => {
+    return this.data[key];
+  };
+
+  // Partial ensures that the props are optional here, but still ensures they are mandatory when creating new User
+  set = (update: Partial<T>): void => {
+    Object.assign(this.data, update)
+  };
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -13,14 +13,4 @@ export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) {}
-
-  get = (propName: string): string | number | undefined => {
-    return this.data[propName as keyof UserProps];
-  };
-
-  // Partial ensures that the props are optional here, but still ensures they are mandatory when creating new User
-  set = (update: Partial<UserProps>): void => {
-    Object.assign(this.data, update)
-  };
 }
