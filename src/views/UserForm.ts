cc704e4812a400c7cdadac12b1e2d59fef4c49c6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-19 21:40:52 +0000

    Added functionality to bind event handlers to document fragment in render method in UserForm class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -11,7 +11,7 @@ export class UserForm {
   };
 
   onButtonClick = (): void => {
-    console.log('Hi there!');
+    console.log('Button clicked');
   };
 
   template = (): string => {
@@ -24,10 +24,26 @@ export class UserForm {
     `;
   };
 
+  // Helper method that takes a reference of a document fragment and parses and binds the relevant event handlers
+  bindEvents = (fragment: DocumentFragment): void => {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      // Destructure the first and second elements in the split array and call them eventName and selector
+      const [eventName, selector] = eventKey.split(':');
+      // Select all relevant elements in the fragment and bind the relevant handler method to it
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  };
+
   render = (): void => {
     // Create a template element, set its inner HTMl to the string rendered in the template method
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    // Bind event handlers to the templateElement content
+    this.bindEvents(templateElement.content);
     // Append the templateElement content to the parent element - content is the actual HTML that gets appended
     this.parent.append(templateElement.content);
   };
