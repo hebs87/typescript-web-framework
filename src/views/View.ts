b7e18998bffa476b86aa5251af020dc61cfc6d78
sunny <sunny@jbcole.co.uk>
2021-01-22 23:36:52 +0000

    Added mapRegions method in View class and called in render method to map elements to specified regions

--- src/views/View.ts
+++ src/views/View.ts
@@ -4,6 +4,9 @@ import {Callback} from "../models/Eventing";
 // T will be the User class that is passed in, and we will also get the UserProps (K), which will be passed into the
 // generic Model class - T will have all the properties of the Model class, with K loaded into it
 export abstract class View<T extends Model<K>, K> {
+  // This will get filled with the relevant regions that we ultimately want to map our templates to
+  regions: {[key: string]: Element} = {};
+
   constructor(
     public parent: Element,
     public model: T
@@ -14,6 +17,11 @@ export abstract class View<T extends Model<K>, K> {
 
   // abstract methods tell us that this method will be passed in when the class is initialised
   abstract template(): string;
+
+  // Dummy regionsMap method to all mapping elements/template to specific regions in UserEdit template
+  regionsMap = (): {[key: string]: string} => {
+    return {};
+  };
 
   // Create dummy method to allow it to be overridden by child classes
   eventsMap = (): {[key: string]: Callback} => {
@@ -40,6 +48,20 @@ export abstract class View<T extends Model<K>, K> {
     }
   };
 
+  // Helper method that takes a reference of a document fragment and parses and binds the relevant region maps
+  mapRegions = (fragment: DocumentFragment): void => {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      // Get the value at this key and set this as the regions object key, then set the matching element as the value
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  };
+
   render = (): void => {
     // Clear parent innerHTML each time the render method is called to prevent multiple elements being rendered
     this.parent.innerHTML = '';
@@ -48,6 +70,8 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template();
     // Bind event handlers to the templateElement content
     this.bindEvents(templateElement.content);
+    // Bind regions to the templateElement content
+    this.mapRegions(templateElement.content);
     // Append the templateElement content to the parent element - content is the actual HTML that gets appended
     this.parent.append(templateElement.content);
   };
