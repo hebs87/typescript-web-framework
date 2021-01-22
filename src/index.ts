91c4eacb2c4b017bf7c4cfe4fe64cd63873880ab
sunny <sunny@jbcole.co.uk>
2021-01-22 23:46:05 +0000

    Added regionsMap to UserEdit class with key value pairs of target elements

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import {User} from "./models/User";
-import {UserForm} from "./views/UserForm";
+import {UserEdit} from "./views/UserEdit";
 
 const user = User.buildUser({name: 'Bruno', age: 1});
 
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(
+  const userEdit = new UserEdit(
     root,
     user
   );
 
-  userForm.render();
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('Root not found!')
 }
