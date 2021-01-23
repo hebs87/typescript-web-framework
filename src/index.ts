9e80ce8922188254cdc865c2b215ab40c077168f
sunny <sunny@jbcole.co.uk>
2021-01-23 00:12:34 +0000

    Added CollectionView and UserList classes to render user collection in HTML template

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
-import {User} from "./models/User";
-import {UserEdit} from "./views/UserEdit";
+// import {User} from "./models/User";
+// import {UserEdit} from "./views/UserEdit";
+//
+// const user = User.buildUser({name: 'Bruno', age: 1});
+//
+// const root = document.getElementById('root');
+//
+// if (root) {
+//   const userEdit = new UserEdit(
+//     root,
+//     user
+//   );
+//
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root not found!')
+// }
 
-const user = User.buildUser({name: 'Bruno', age: 1});
+import {UserList} from "./views/UserList";
+import {Collection} from "./models/Collection";
+import {User, UserProps} from "./models/User";
 
-const root = document.getElementById('root');
+const users = new Collection('http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json)
+  }
+);
 
-if (root) {
-  const userEdit = new UserEdit(
-    root,
-    user
-  );
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root not found!')
-}
+users.fetch();
